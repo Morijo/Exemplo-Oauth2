c586e835e37301f64f2b01d190abff344715b1ff
root <root@localhost.localdomain>
2017-01-12 22:44:17 -0200

    Exemplo de Autenticação Oauth2

---
+++ config.go
@@ -0,0 +1,10 @@
+package main
+
+var (
+	APIlistenPath string = "cep"
+	orgID         string = "586d1c5f84f82d00012fce0a"
+	policyID      string = "586d24d284f82d00012fce13"
+	BaseAPIID     string = "56be1fe11b0344694bf931ef527bc6e3"
+	GatewayHost   string = "http://127.0.0.1:8080"
+	AdminSecret   string = "352d20ee67be67f6340b4c0605b044b7"
+)
